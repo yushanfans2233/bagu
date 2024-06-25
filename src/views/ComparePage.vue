30359627408b52732670d1a535ebc5d46d27ce5e
yushan <yushanfans2233@qq.com>
2024-06-25 18:45:51 +0800

    Complete comparison

--- src/views/ComparePage.vue
+++ src/views/ComparePage.vue
@@ -1,27 +1,82 @@
 <template>
-  <div>
-    <textarea v-model="text1"></textarea>
-    <textarea v-model="text2"></textarea>
-    <div v-if="isSame">Same</div>
-    <div v-else>Different</div>
+  <div class="monaco-editor-container">
+    <h1>文本1</h1>
+    <div ref="originalEditorContainer" class="monaco-editor"></div>
+    <h1>文本2</h1>
+    <div ref="modifiedEditorContainer" class="monaco-editor"></div>
+    <h1>对比</h1>
+    <div ref="diffEditorContainer" class="monaco-editor"></div>
   </div>
 </template>
 
-<script>
-import { ref, computed } from 'vue'
+<script setup>
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import {onMounted, ref, watch} from 'vue';
 
-export default {
-  setup() {
-    const text1 = ref('')
-    const text2 = ref('')
+const originalText = ref('');
+const modifiedText = ref('');
 
-    const isSame = computed(() => text1.value === text2.value)
+const originalEditorContainer = ref(null);
+const modifiedEditorContainer = ref(null);
+const diffEditorContainer = ref(null);
 
-    return {
-      text1,
-      text2,
-      isSame
-    }
-  }
+let originalEditor, modifiedEditor, diffEditor;
+
+onMounted(() => {
+  originalEditor = monaco.editor.create(originalEditorContainer.value, {
+    value: originalText.value,
+    language: 'text',
+    theme: 'vs-dark',
+    minimap: {enabled: false},
+  });
+
+  modifiedEditor = monaco.editor.create(modifiedEditorContainer.value, {
+    value: modifiedText.value,
+    language: 'text',
+    theme: 'vs-dark',
+    minimap: {enabled: false},
+  });
+
+  diffEditor = monaco.editor.createDiffEditor(diffEditorContainer.value, {
+    enableSplitViewResizing: false,
+    readOnly: true,
+    theme: 'vs-dark',
+    minimap: {enabled: false},
+  });
+
+  originalEditor.onDidChangeModelContent(() => {
+    updateDiff();
+  });
+
+  modifiedEditor.onDidChangeModelContent(() => {
+    updateDiff();
+  });
+});
+
+watch([originalText, modifiedText], () => {
+  updateDiff();
+});
+
+function updateDiff() {
+  const originalModel = monaco.editor.createModel(originalEditor.getValue(), 'text/plain');
+  const modifiedModel = monaco.editor.createModel(modifiedEditor.getValue(), 'text/plain');
+
+  diffEditor.setModel({
+    original: originalModel,
+    modified: modifiedModel,
+  });
 }
 </script>
+
+<style scoped>
+.monaco-editor-container {
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  width: 1200px;
+}
+
+.monaco-editor {
+  flex: 1;
+}
+</style>
