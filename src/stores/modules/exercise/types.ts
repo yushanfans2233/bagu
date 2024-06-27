488d56f04a1a2d7041dfbb184dcc8e964807048e
Henry Zhang <imentu@foxmail.com>
2024-06-27 20:15:30 +0800

    feat: useExerciseStore

---
+++ src/stores/modules/exercise/types.ts
@@ -0,0 +1,11 @@
+export class Exercise {
+  constructor(id: string, title: string, content: string) {
+    this.id = id
+    this.title = title
+    this.content = content
+  }
+
+  id: string
+  title: string
+  content: string
+}
