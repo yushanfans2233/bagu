53fb35fc503ba837e72d30c382315ee7bac8ecc6
Henry Zhang <imentu@foxmail.com>
2024-07-14 19:35:29 +0800

    feat: analytics

--- vite.config.ts
+++ vite.config.ts
@@ -17,23 +17,23 @@ const AddAnalytics = (isProduction: boolean): PluginOption => {
           '<!-- Microsoft Clarity -->',
           `
 <script type="text/javascript">
-  (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  })(window, document, "clarity", "script", "mzstl94s97");
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "mzstl94s97");
 </script>
 `,
         ).replace(
           '<!-- Google Analytics -->',
           `
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-36GCMM73V3"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XTNKYNPBZF"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-36GCMM73V3');
+  gtag('config', 'G-XTNKYNPBZF');
 </script>
 `
         )
